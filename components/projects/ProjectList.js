5684834fef135f2d58b2ee6203a29c41c1d231f3
Mayank Kulkarni <31683839+mayankk245@users.noreply.github.com>
2019-12-05 10:59:18 +0530

    Dashboard Updated
    Dashboard updated with required components

---
+++ components/projects/ProjectList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const ProjectList = () => {
+return(
+
+  <div className="project-list section">
+ 
+  <div className="card z-depth-0 project-summary">
+  <div className="card-content grey-text text-darken-3">
+  <span className="card-title ">Project title</span>
+  <p>post by Mayank kulkarni</p>
+  <p className="grey-text">5th Dec 19</p>
+  
+  </div>
+  </div>
+  
+   <div className="card z-depth-0 project-summary">
+  <div className="card-content grey-text text-darken-3">
+  <span className="card-title ">Project title</span>
+  <p>post by Mayank kulkarni</p>
+  <p className="grey-text">5th Dec 19</p>
+  
+  </div>
+  </div>
+  
+   <div className="card z-depth-0 project-summary">
+  <div className="card-content grey-text text-darken-3">
+  <span className="card-title ">Project title</span>
+  <p>post by Mayank kulkarni</p>
+  <p className="grey-text">5th Dec 19</p>
+  
+  </div>
+  </div>
+  
+  
+  
+  </div>
+)
+
+}
+export default ProjectList;
